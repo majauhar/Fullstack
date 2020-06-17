99d297b9c0decee6bb8ba3f621711063e1519391
Jauhar <siddmohamed19@gmail.com>
2020-06-17 09:45:11 +0530

    removed previous folders

---
+++ part1/part1-intermediate/1_nine.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Statistics = (props) => {
+  const {good, bad, neutral} = props
+  console.log(good + bad + neutral)
+  if (good + bad + neutral === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  return (
+    <div>
+    <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good - bad) /(good + neutral + bad)}</p>
+      <p>positive {good * 100 / (good + neutral + bad)} %</p>
+    </div>
+  )
+
+}
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+
+      <button onClick={() => setGood(good + 1)} >good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
